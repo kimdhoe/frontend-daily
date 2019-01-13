72fe69f159fecc28d7ffce7c7cef7fd94a3da2de
Donghee Kim <kimdhoe1@gmail.com>
2019-01-13 23:27:08 +0900

    change link url to publication url

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,49 +16,49 @@ const IndexPage = () => (
         number={8}
         date={'2019. 01. 13'}
         note={'Transducers: Efficient data processing pipelines in JavaScript'}
-        url={'https://medium.com/@kimdhoe1/8-transducers-efficient-data-processing-pipelines-in-javascript-96afb0bfd4b5'}
+        url={'https://medium.com/frontend-daily/8-transducers-efficient-data-processing-pipelines-in-javascript-96afb0bfd4b5'}
       />
       <Item
         number={7}
         date={'2019. 01. 12'}
         note={'Defensive JavaScript'}
-        url={'https://medium.com/@kimdhoe1/7-defensive-javascript-46bf4172e273'}
+        url={'https://medium.com/frontend-daily/7-defensive-javascript-46bf4172e273'}
       />
       <Item
         number={6}
         date={'2019. 01. 11'}
         note={'Linked lists in the wild: React Hooks'}
-        url={'https://medium.com/@kimdhoe1/6-linked-lists-in-the-wild-react-hooks-68082d59d3bc'}
+        url={'https://medium.com/frontend-daily/6-linked-lists-in-the-wild-react-hooks-68082d59d3bc'}
       />
       <Item
         number={5}
         date={'2019. 01. 10'}
         note={'A comprehensive look back at front-end in 2018'}
-        url={'https://medium.com/@kimdhoe1/5-a-comprehensive-look-back-at-front-end-in-2018-f08b852b4e97'}
+        url={'https://medium.com/frontend-daily/5-a-comprehensive-look-back-at-front-end-in-2018-f08b852b4e97'}
       />
       <Item
         number={4}
         date={'2019. 01. 09'}
         note={'Introducing React Hooks'}
-        url={'https://medium.com/@kimdhoe1/4-introducing-hooks-db43f3be428f'}
+        url={'https://medium.com/frontend-daily/4-introducing-hooks-db43f3be428f'}
       />
       <Item
         number={3}
         date={'2019. 01. 08'}
         note={'Better reusable React components with the overrides pattern'}
-        url={'https://medium.com/@kimdhoe1/3-better-reusable-react-components-with-the-overrides-pattern-bee73a42428e'}
+        url={'https://medium.com/frontend-daily/3-better-reusable-react-components-with-the-overrides-pattern-bee73a42428e'}
       />
       <Item
         number={2}
         date={'2019. 01. 07'}
         note={'A unified styling language'}
-        url={'https://medium.com/@kimdhoe1/2-a-unified-styling-language-b957e4daa8ea'}
+        url={'https://medium.com/frontend-daily/2-a-unified-styling-language-b957e4daa8ea'}
       />
       <Item
         number={1}
         date={'2019. 01. 06'}
         note={'Things I don\'t know'}
-        url={'https://medium.com/@kimdhoe1/1-things-i-dont-know-3dcc616811ef'}
+        url={'https://medium.com/frontend-daily/1-things-i-dont-know-3dcc616811ef'}
       />
     </ul>
 
